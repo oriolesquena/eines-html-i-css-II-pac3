c8cf37aa48828e6781e6b7297d7d0de9606423f1
[Oriol Esquena Calm] <oesquena@uoc.edu>
2023-06-02 00:18:33 +0200

    pagina campionat finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
+    colors: {
+      'dark-green': '#001f06',
+      'light-green': '#c8eba6',
+      'light-gray': '#d5dcd7',
+      lime: colors.lime,
+      green: colors.green,
+    },
   },
   plugins: [],
 };
