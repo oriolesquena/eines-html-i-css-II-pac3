e1c6a0891d7f8c95f69f66fe2738ff26b01ed666
[Oriol Esquena Calm] <oesquena@uoc.edu>
2023-06-01 15:49:13 +0200

    header and footer

--- src/assets/scripts/img.js
+++ src/assets/scripts/img.js
@@ -10,24 +10,22 @@
  * Write any other JavaScript below
  */
 
-import * as bootstrap from 'bootstrap';
-
-var modal = document.getElementById("myModal");
+var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById('img01');
+var captionText = document.getElementById('caption');
+img.onclick = function () {
+  modal.style.display = 'block';
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
-}
+};
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName('close')[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+span.onclick = function () {
+  modal.style.display = 'none';
+};
